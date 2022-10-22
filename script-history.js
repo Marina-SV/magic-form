623bb0e94fddf4a94b4620d058c499c67ee95a23
Senina Marina <frau.senina32@yandex.ru>
2022-10-22 17:16:58 +0300

    initial project

---
+++ script-history.js
@@ -0,0 +1,63 @@
+let mainHistoryContent;
+let submittedValuesArray;
+let content = "";
+
+createPageContent();
+window.addEventListener("storage", createPageContent);
+
+function createPageContent() {
+    retrieveFromLocalStorage();
+    getHistoryCards();
+    showCards();
+}
+
+function retrieveFromLocalStorage(){
+    mainHistoryContent = document.getElementById("main-history");
+    submittedValuesArray = JSON.parse(localStorage.getItem("submittedValues") || "[]");
+    content = "";
+}
+
+function getHistoryCardHtml(i) {
+    return `<div class="submit-history-card">
+      <p class="history-card-title">First Name</p>
+      <p class="history-card-info card-first-name">${submittedValuesArray[i].firstName}</p> 
+      
+      <p class="history-card-title">Last Name</p>
+      <p class="history-card-info card-last-name">${submittedValuesArray[i].lastName}</p>
+      
+      <p class="history-card-title">Email</p>
+      <p class="history-card-info card-email">${submittedValuesArray[i].email}</p> 
+      
+      <p class="history-card-title">Phone</p>
+      <p class="history-card-info card-phone">${submittedValuesArray[i].phone}</p>
+      
+      <p class="history-card-title">Company</p>
+      <p class="history-card-info card-company">${submittedValuesArray[i].company}</p> 
+      
+      <p class="history-card-title">Adress</p>
+      <p class="history-card-info card-address">${submittedValuesArray[i].address}</p>  
+      
+      <div class="delete-button-container-div">
+      <button type="submit" class="delete-button" onClick="deleteCard(${i})">Delete</button>         
+      </div>
+   </div>`
+}
+function getHistoryCards(){
+    for (let i = 0; i < submittedValuesArray.length; i++) {
+        content = content + getHistoryCardHtml(i);
+    }
+}
+
+function showCards(){
+    mainHistoryContent.innerHTML = content;
+}
+
+function deleteCard(index) {
+    submittedValuesArray.splice(index,1);
+    localStorage.setItem("submittedValues", JSON.stringify(submittedValuesArray));
+
+    //location.reload();  Error: Execution context was destroyed, most likely because of a navigation.
+
+    createPageContent();
+
+}
---
+++ scripts.js
@@ -0,0 +1,88 @@
+let firstNameElement = document.getElementById("first-name");
+let lastNameElement = document.getElementById("last-name");
+let emailElement = document.getElementById("email");
+let phoneElement = document.getElementById("phone");
+let companyElement = document.getElementById("company");
+let addressElement = document.getElementById("address");
+
+let buttonElement = document.getElementById("submit-button")
+
+fillForm();
+window.addEventListener("storage", fillForm);
+
+firstNameElement.addEventListener("input",saveFirstName);
+lastNameElement.addEventListener("input",saveLastName);
+emailElement.addEventListener("input",saveEmail);
+phoneElement.addEventListener("input",savePhone);
+companyElement.addEventListener("input",saveCompany);
+addressElement.addEventListener("input",saveAddress);
+
+buttonElement.addEventListener("click", clickSubmitButton);
+
+
+function saveFirstName() {
+    localStorage.setItem("first-name",firstNameElement.value);
+}
+function saveLastName() {
+    localStorage.setItem("last-name",lastNameElement.value);
+}
+function saveEmail() {
+    localStorage.setItem("email",emailElement.value);
+}
+function savePhone() {
+    localStorage.setItem("phone",phoneElement.value);
+}
+function saveCompany() {
+    localStorage.setItem("company",companyElement.value);
+}
+function saveAddress() {
+    localStorage.setItem("address",addressElement.value);
+}
+
+function fillForm()
+{
+    firstNameElement.value = localStorage.getItem("first-name");
+    lastNameElement.value = localStorage.getItem("last-name");
+    emailElement.value = localStorage.getItem("email");
+    phoneElement.value = localStorage.getItem("phone");
+    companyElement.value = localStorage.getItem("company");
+    addressElement.value = localStorage.getItem("address");
+}
+
+function clickSubmitButton(){
+    addToSubmittedValuesArray();
+    emptySubmittedInfo(); // зачем?
+
+}
+
+function addToSubmittedValuesArray() {
+    let submittedValuesArray = JSON.parse(localStorage.getItem("submittedValues") || "[]");
+// submittedValues вернет null и JSON.parse('[]') просто объявит массив
+    submittedValuesArray.push(createValuesObject());
+    localStorage.setItem("submittedValues", JSON.stringify(submittedValuesArray));
+    console.log(JSON.stringify(submittedValuesArray));
+}
+
+// console.log(JSON.stringify(submittedValuesArray));
+
+function emptySubmittedInfo() {
+    localStorage.setItem("first-name","");
+    localStorage.setItem("last-name","");
+    localStorage.setItem("email","");
+    localStorage.setItem("phone","");
+    localStorage.setItem("company","");
+    localStorage.setItem("address","");
+}
+
+console.log(localStorage)
+
+function createValuesObject() {
+    return  {
+        "firstName":firstNameElement.value,
+        "lastName":lastNameElement.value,
+        "email":emailElement.value,
+        "phone":phoneElement.value,
+        "company":companyElement.value,
+        "address":addressElement.value
+    }
+}
